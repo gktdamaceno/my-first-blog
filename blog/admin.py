3f07340841384249589ae79d9da221efcd0c2af4
gktdamaceno <gktdamaceno@gmail.com>
2018-09-01 11:30:49 +1000

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+*.pyc
+*~
+__pycache__
+myvenv
+db.sqlite3
+/static
+.DS_Store
+
+# Register your models here.
