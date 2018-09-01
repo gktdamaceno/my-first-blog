26ba9eb94117a88dcc26f3aa04bafdb6f8e514e7
gktdamaceno <gktdamaceno@gmail.com>
2018-09-01 13:19:00 +1000

    gktdamaceno
    I made a few changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xykhjgsq-!r)&zdcpz$&=(zt#7z9q2ofb7u5^f)uhc*6onh7#@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-'django.contrib.messages.storage.session.SessionStorage'
+
+#'django.contrib.messages.storage.session.SessionStorage'
